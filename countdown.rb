2ee5b0c7ee334b3d3714296ea11817f376056e35
exuberant-state-1364 <jessicahong.ngnr@gmail.com>
2019-05-13 18:45:49 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(number)
+ while number >= 1
+  puts "#{number} SECOND(S)!"
+ number -= 1
+end
+  "HAPPY NEW YEAR!"
+end 
+
+def countdown_with_sleep (time)
+  while time > 0
+ puts "#{time} SECOND(S)!"
+  sleep (1)
+  time -= 1
+
+ end
 end
